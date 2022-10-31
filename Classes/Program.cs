3ffde78fd9273098c7868c18568f9ada05644ba0
tyler ambrose <blitzinc98@yahoo.com>
2022-10-30 21:21:56 -0400

    finshed exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Classes
+﻿using System.Xml.Schema;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var Tesla = new Car();
+            Tesla.Make = "Tesla";
+            Tesla.Model = "3";
+            Tesla.Year = 2022;
+            Console.WriteLine($"I crashed a {Tesla.Make} Model {Tesla.Model} {Tesla.Year}. :(");
         }
     }
 }
